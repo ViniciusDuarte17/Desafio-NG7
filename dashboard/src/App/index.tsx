7b0a9c0a2114ece8563e173b5c5014b1797aa06d
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-05-17 09:37:24 -0300

    feat: Configurando react para template com typescript/ styled-components

---
+++ dashboard/src/App/index.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from "../GlobalStyled"
+
+function App() {
+
+    return (
+      <>
+        <GlobalStyles />
+        <h1>Configuração inicial do dashboard</h1>
+      </>
+    )
+  }
+  
+  export default App
