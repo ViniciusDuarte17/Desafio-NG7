9b82fe93339e06532ab06a9081cfcaa8ddb25c22
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-05-18 11:14:38 -0300

    refactor: Formantando código css do componente Sidebar

--- dashboard/src/components/SideBar/styled.ts
+++ dashboard/src/components/SideBar/styled.ts
@@ -1,37 +1,37 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: #15152b;
+
+  ul {
+    list-style: none;
+    height: 50vh;
     display: flex;
     flex-direction: column;
+    margin-bottom: 13vh;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+
+  li {
     width: 100%;
-    height: 100vh;
-    background-color:#15152B;
-
-    ul {
-        list-style: none;
-        height: 50vh;
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 13vh;
-        align-items: center;
-        justify-content: space-evenly;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:focus,
+    &:hover {
+      cursor: pointer;
+      background-color: #090e16;
     }
+  }
 
-    li {
-        width: 100%;
-        height: 70px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        &:focus, &:hover{
-            cursor: pointer;
-            background-color: #090E16;
-        }
-    }
-
-    a {
-        text-decoration: none;
-        border: none;
-    }
-`
+  a {
+    text-decoration: none;
+    border: none;
+  }
+`;
